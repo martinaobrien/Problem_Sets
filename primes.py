5dfac68a543be01ec95516bcc0108386d6c0f696
martinaobrien <47950332+martinaobrien@users.noreply.github.com>
2019-03-10 12:29:55 +0000

    Add files via upload

--- primes.py
+++ primes.py
@@ -2,10 +2,12 @@
 #Problem St 5 primes
 #Calculation of whether or not a positive number is a prime number
 
-start = int (input ('Enter Number: '))
-if start > 1:
-    print ('positive interger')
-
+number = int (input ('Enter Number: '))
+if number > 1:
+    for i in range (2, number):
+        if (number % i) == 0:
+            print ('This is a prime number')
+            break
 else: 
     print ('This is not a prime number')
 
