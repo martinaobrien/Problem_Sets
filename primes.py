856ec4e5c17e6ec61960b434e4e318c9f3a1691a
martinaobrien <47950332+martinaobrien@users.noreply.github.com>
2019-03-10 11:52:25 +0000

    Add files via upload

---
+++ primes.py
@@ -0,0 +1,11 @@
+#Martna O'Brien 10 - 03 - 2019
+#Problem St 5 primes
+#Calculation of whether or not a positive number is a prime number
+
+start = int (input ('Enter Number: '))
+if start > 1:
+    print ('positive interger')
+
+else: 
+    print ('This is not a prime number')
+
