50b59434f3a8cbfc2ddfb7b0d92f4692ad6b996a
martinaobrien <47950332+martinaobrien@users.noreply.github.com>
2019-03-26 21:49:13 +0000

    Add files via upload
    Additional code added to ensure the inputted integer is a positive number

--- sumupto.py
+++ sumupto.py
@@ -7,19 +7,24 @@
 # input "Enter Number" will be visible on the user interface
 # setting up variables to be used later in while loop
 
-start = int (input ('Enter Number: '))
+start = int (input ('Please enter a positive number: '))
 ans = 0
 # ans = 0 and 0 is the value that the loop begins calculating
 i = 1
-# i = 1 as the starting point
-while i <= start:
-    ans = ans + i
-    i = i + 1
+if start > 0:
+ # i = 1 as the starting point
+    while i <= start:
+        ans = ans + i
+        i = i + 1
+    print(ans)
+# Print is the funtion used to display the sum of all factorials of positive integer inputted
+# As print is not contained through indentation in the previous statement, it displays one figure as ans
 
+else: 
+    print('This is not a positive number')
+    
 # ans will continue to increase by the value of i as it progresses through the while loop
 # i will increase by 1 each time it goes through while loop.
-# This will continue until the value i reaches the start number
+# This will continue while the argument is true i.e: Until the i reaches the start number.
 
-print(ans)
-# Print is the funtion used to display the sum of all factorials of positive integer inputted
-# As print is not contained through indentation in the previous, it displays one figure as ans
+
