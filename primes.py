392fe6b3ad3ad778c9c0fdaaf35485fc3b68cad6
martinaobrien <47950332+martinaobrien@users.noreply.github.com>
2019-03-27 23:40:06 +0000

    Update primes.py
    Updated with comments for code and more defined code to ensure the number inputted is valid i.e. a positive integer and not 1

--- primes.py
+++ primes.py
@@ -3,11 +3,23 @@
 #Calculation of whether or not a positive number is a prime number
 
 number = int (input ('Enter Number: '))
-if number > 1:
-    for i in range (2, number):
-        if (number % i) == 0:
-            print ('This is a prime number')
-            break
-else: 
-    print ('This is not a prime number')
+if number > 0:
+    if number > 1: 
+        for i in range (2, number): # use of for loop to repeatedly execute some code statement
+             if (number % i) == 0: # this argument will check if the range of numbers between 2 and that number, checking each potential factor
+                print ('This is a prime number')
+                break # breaks out of the loop entirely
+        else: # else statement is executed  if not of the factors divide the inputted number
+            print(number,"is a prime number")
+    else: #else statement is executed if integer is 1
+        print ('This is not a prime number')
+else: #else statement is executed if the inputted integer is not positive
+        print ('This is not a positive integer')
+
+
+    
+
+
+
+       
 
