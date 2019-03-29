e09ed69d8cf6627faba61e375817dc53a640910b
martinaobrien <G00376463@gmit.ie>
2019-03-29 23:21:04 +0000

    Update collatz.py
    Addition of while loop, need to debug output code

--- collatz.py
+++ collatz.py
@@ -8,20 +8,21 @@
 # Input will then be calculated as per loop statements
 # setting up variables to be used later in while loop
 
-i = int (input ('Enter Number: '))
+i = input ('Enter Number: ')
 
-if i >= 0: # integer has to be greater than 0, the if statement investigates whether an argument is true
-    if i %2== 0: # this determines whether or not the integer is even
-        print (i//2) #determines if the number is even
-    elif i %2== 1:
-        print (i *3 + 1)
+#if i >= 0: # integer has to be greater than 0, the if statement investigates whether an argument is true
+    def collatz (number): #a python program to 
+        if number %2== 0: # this determines whether or not the integer is even
+            print (number//2) #determines if the number is even
+            return number//2
 
+        elif number %2== 1:
+            result= (number *3 + 1)
+             print (result)
+             return result
 
+    while i != 1:
+        i = collatz(int(i))
 
-
-    else: 
-        print ('False')
-
-
-else: 
-    print('This is not a positive number')
+#else: 
+ #   print('This is not a positive number')
