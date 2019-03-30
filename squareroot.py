847bc3b5d72c3a511e7c6736e51e62966d69e64a
martinaobrien <G00376463@gmit.ie>
2019-03-30 14:26:42 +0000

    Update squareroot.py

--- squareroot.py
+++ squareroot.py
@@ -5,8 +5,11 @@
 import math # math function is imported
 num = int(input ('Enter positive number: ')) #variable for the following code
 
-if num >= 0: # integer has to be greater than 0, the if statement investigates whether an argument is trus
-    print (format(math.sqrt(num), '.2f'))   # give 2 digits after the point
+if num >= 0:
+    #integer has to be greater than 0, the if statement investigates whether an argument is trus
+    print (format(math.sqrt(num), '.2f'))  
+    # give 2 digits after the point
 else:
-    print ('This is not a positive number') # else statement will be executed if the if statement if proved to be false
+    print ('This is not a positive number') 
+    # else statement will be executed if the if statement if proved to be false
 
