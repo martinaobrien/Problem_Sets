80a9cd97efbbde06675a1c7399b86b3b714cb432
martinaobrien <G00376463@gmit.ie>
2019-03-30 09:21:35 +0000

    Update collatz.py
    Final coding for program

--- collatz.py
+++ collatz.py
@@ -6,24 +6,24 @@
 # Int method returns an integer as per python programming
 # input "Enter Number" will be visible on the user interface
 # Input will then be calculated as per loop statements
-# setting up variables to be used later in while loop
+# Setting up variables to be used later in while loop
 # Reference for code: adapted from https://www.webucator.com/blog/2015/07/collatz-conjecture-in-python/
 
 i = int(input ('Enter Number: '))
 
 if i >= 0: # integer has to be greater than 0, the if statement investigates whether an argument is true
-    def collatz(number): # a python program to 
-        if number % 2 == 0: # this determines whether or not the integer is even
+    def collatz(number): 
+        if number % 2 == 0: # determines whether or not the integer is even
             print(number // 2) 
-            return number // 2 # function the control to the function that callled it rather than ending the function
+            return number // 2 # function returns the control to the function that callled it rather than ending the function
 
-        elif number % 2 == 1:
-            result = 3 * number + 1
+        elif number % 2 == 1: # i is odd and is calculated as per programming
+            result = 3 * number + 1 
             print(result)
-            return result
+            return result #function returns the control to the function that callled it rather than ending the function
 
 
     while i != 1:
-        i = collatz(int(i))
+        i = collatz(int(i)) # collates the numbers output in the loop statement
 else: 
     print('This is not a positive number')
