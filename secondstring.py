c6ceb2bdcf1056d40e29822c82f8750457cecaf6
martinaobrien <G00376463@gmit.ie>
2019-03-30 00:28:30 +0000

    Update secondstring.py
    First attempt at split string

--- secondstring.py
+++ secondstring.py
@@ -1,1 +1,12 @@
+# Martina O'Brien - 25 - 03 - 2019
+# Problem Set Programming and Scripting Code 6
+# Output gives selected words in a string
+
+# Input string needed for programme
+
+i = str(input ('Enter your text here":))
+
+even_words = str split ()
+
+print even_words ([::2])
 
