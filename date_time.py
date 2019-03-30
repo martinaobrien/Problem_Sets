1a18173e2f66ec069360bea9620b6c9391ff3dfc
martinaobrien <G00376463@gmit.ie>
2019-03-30 14:26:07 +0000

    Create date_time.py

---
+++ date_time.py
@@ -0,0 +1,14 @@
+from datetime import datetime as dt
+
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
+def time.strftime(format, t):
+    return t.strftime (format %Y %-I )
+
+x = custom_strftime('%A, %B {S}, %Y %-I', dt.now())
+
+print (x)
