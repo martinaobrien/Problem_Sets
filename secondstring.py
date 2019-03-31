47c4377bf5d34a33330c89adc66c6b9c4981516a
martinaobrien <G00376463@gmit.ie>
2019-03-31 00:11:39 +0000

    Update secondstring.py
    Final Version

--- secondstring.py
+++ secondstring.py
@@ -12,7 +12,7 @@ every_second_word = str.split()[::2]
 #Reference:https://www.geeksforgeeks.org/python-string-split/
 #Reference:https://www.pythonforbeginners.com/dictionary/python-split 
 
-print (''.join(every_second_word))
+print (' '.join(every_second_word))
 # Using ''.join takes the quotation marks and commas away from the output, constructing a new sentence. 
 # '' denotes was will be outputted between each of the words
 # Reference: https://www.tutorialspoint.com/python3/string_join.htm
